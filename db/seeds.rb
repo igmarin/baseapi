250be02e409a59cfeb71b4509ec44d0436cfd23f
Ismael G Marin C <ismael.marin@gmail.com>
2015-08-07 13:35:29 -0500

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
+Code.delete_all
 Code.create(promo_code: "free", partner_id: "samsclub")
 Code.create(promo_code: "", partner_id: "nestle")
 Code.create(promo_code: "10", partner_id: "cocacola")
 Code.create(promo_code: "", partner_id: "modelo")
+Press.delete_all
 Press.create(avatar: "/images/partners/logos/yahoo.svg",   partner_name: "Yahoo Commerce Central", link_uri: "")
 Press.create(avatar: "/images/partners/logos/multi.svg",   partner_name: "Multifunding", link_uri: "")
 Press.create(avatar:"/images/partners/logos/funding.svg",  partner_name: "Funding Circle", link_uri: "")
